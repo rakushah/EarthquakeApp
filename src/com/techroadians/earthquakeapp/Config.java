2dbe2c68b56de290f2a138a988a414e0b8ea0271
rakushah <rakushah123@gmail.com>
2015-05-07 16:54:23 +0545

    changes done to add captured image

---
+++ src/com/techroadians/earthquakeapp/Config.java
@@ -0,0 +1,10 @@
+package com.techroadians.earthquakeapp;
+
+public class Config {
+	// File upload url (replace the ip with your server address)
+	public static final String FILE_UPLOAD_URL = " http://192.168.0.110/equake/uploads";
+	
+	// Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
+}
+
--- src/com/techroadians/earthquakeapp/MainActivity.java
+++ src/com/techroadians/earthquakeapp/MainActivity.java
@@ -1,27 +1,314 @@
 package com.techroadians.earthquakeapp;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Images;
+import android.provider.MediaStore.MediaColumns;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.techroadians.earthquakeapp.AndroidMultiPartEntity.ProgressListener;
 
 //this is a change
 
 public class MainActivity extends ActionBarActivity {
+	ImageView captureImage;
 
+	private String filePath = null;
+	long totalSize = 0;
+	// LogCat tag
+	private static final String TAG = MainActivity.class.getSimpleName();
+	
+ 
+    // Camera activity request codes
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
+  
+    
+    public static final int MEDIA_TYPE_IMAGE = 1;
+   
+ 
+    Uri fileUri; // file url to store image/video
+    String picturePath;
+    Uri selectedImage;
+    Bitmap photo;
+	private TextView txtPercentage;
+	private ProgressBar progressBar;
+	private String imgPath;
+
+    String ba1;
+    public static String URL = "Paste your URL here";
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Log.d("hello", "hello");
+		txtPercentage = (TextView) findViewById(R.id.txtPercentage);
+		progressBar = (ProgressBar) findViewById(R.id.progressBar);
+		captureImage = (ImageView) findViewById(R.id.ivUploadPhoto);
+		captureImage.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				captureImage();
+				//Toast.makeText(getApplicationContext(), "ImageView Clicked", Toast.LENGTH_LONG).show();
+			}
+		});
+		// Checking camera availability
+        if (!isDeviceSupportCamera()) {
+            Toast.makeText(getApplicationContext(),
+                    "Sorry! Your device doesn't support camera",
+                    Toast.LENGTH_LONG).show();
+            // will close the app if the device does't have camera
+            finish();
+        }
+		
 	}
+	  /**
+     * Checking device has camera hardware or not
+     * */
+    private boolean isDeviceSupportCamera() {
+        if (getApplicationContext().getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_CAMERA)) {
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+    /**
+     * Launching camera app to capture image
+     */
+    private void captureImage() {
+    	   Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    	   
+    	   
+           intent.putExtra(MediaStore.EXTRA_OUTPUT, setImageUri());
 
+           // start the image capture Intent
+           startActivityForResult(intent, 100);
+    }
+    public Uri setImageUri() {
+        // Store image in dcim
+        File file = new File(Environment.getExternalStorageDirectory() + "/DCIM/", "image" + new Date().getTime() + ".png");
+        Uri imgUri = Uri.fromFile(file);
+        this.imgPath = file.getAbsolutePath();
+        return imgUri;
+    }
+
+
+    public String getImagePath() {
+        return imgPath;
+    }
+
+
+public String getAbsolutePath(Uri uri) {
+        String[] projection = { MediaColumns.DATA };
+        @SuppressWarnings("deprecation")
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        if (cursor != null) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaColumns.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } else
+            return null;
+    }
+
+public Bitmap decodeFile(String path) {
+        try {
+            // Decode image size
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            BitmapFactory.decodeFile(path, o);
+            // The new size we want to scale to
+            final int REQUIRED_SIZE = 70;
+
+            // Find the correct scale value. It should be the power of 2.
+            int scale = 1;
+            while (o.outWidth / scale / 2 >= REQUIRED_SIZE && o.outHeight / scale / 2 >= REQUIRED_SIZE)
+                scale *= 2;
+
+            // Decode with inSampleSize
+            BitmapFactory.Options o2 = new BitmapFactory.Options();
+            o2.inSampleSize = scale;
+            return BitmapFactory.decodeFile(path, o2);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+    /**
+     * Receiving activity result method will be called after closing the camera
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // if the result is capturing Image
+    	  if (requestCode == 100 && resultCode == RESULT_OK) {
+    		  
+    		  String selectedImagePath = getImagePath();
+    	     captureImage.setImageBitmap(decodeFile(selectedImagePath));
+    	  
+    	     // CALL THIS METHOD TO GET THE URI FROM THE BITMAP
+    	       // Uri tempUri = getImageUri(MainActivity.this, image);
+   	        // CALL THIS METHOD TO GET THE ACTUAL PATH
+    	     Toast.makeText(getApplicationContext(), getImagePath(), Toast.LENGTH_LONG).show();
+    	     new UploadFileToServer().execute();
+    	        //File finalFile = new File(getRealPathFromURI(fileUri));
+    	       //Toast.makeText(getApplicationContext(), finalFile.toString(), Toast.LENGTH_LONG).show();
+          }
+    }
+    
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	/**
+	 * Uploading the file to server
+	 * */
+	private class UploadFileToServer extends AsyncTask<Void, Integer, String> {
+		@Override
+		protected void onPreExecute() {
+			// setting progress bar to zero
+			progressBar.setProgress(0);
+			super.onPreExecute();
+		}
+
+		@Override
+		protected void onProgressUpdate(Integer... progress) {
+			// Making progress bar visible
+			progressBar.setVisibility(View.VISIBLE);
+
+			// updating progress bar value
+			progressBar.setProgress(progress[0]);
+
+			// updating percentage value
+			txtPercentage.setText(String.valueOf(progress[0]) + "%");
+		}
+
+		@Override
+		protected String doInBackground(Void... params) {
+			return uploadFile();
+		}
+
+		@SuppressWarnings("deprecation")
+		private String uploadFile() {
+			String responseString = null;
+
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost(Config.FILE_UPLOAD_URL);
+
+			try {
+				AndroidMultiPartEntity entity = new AndroidMultiPartEntity(new ProgressListener() {
+					
+					public void transferred(long num) {
+						// TODO Auto-generated method stub
+						publishProgress((int) ((num / (float) totalSize) * 100));
+						
+					}
+				});
+			
+
+				File sourceFile = new File(getImagePath());
+
+				// Adding file data to http body
+				entity.addPart("image", new FileBody(sourceFile));
+
+//				// Extra parameters if you want to pass to server
+//				entity.addPart("website",
+//						new StringBody("www.androidhive.info"));
+//				entity.addPart("email", new StringBody("abc@gmail.com"));
+
+				totalSize = entity.getContentLength();
+				httppost.setEntity(entity);
+
+				// Making server call
+				HttpResponse response = httpclient.execute(httppost);
+				HttpEntity r_entity = response.getEntity();
+
+				int statusCode = response.getStatusLine().getStatusCode();
+				if (statusCode == 200) {
+					// Server response
+					responseString = EntityUtils.toString(r_entity);
+				} else {
+					responseString = "Error occurred! Http Status Code: "
+							+ statusCode;
+				}
+
+			} catch (ClientProtocolException e) {
+				responseString = e.toString();
+			} catch (IOException e) {
+				responseString = e.toString();
+			}
+
+			return responseString;
+
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			Log.e(TAG, "Response from server: " + result);
+			//Toast.makeText(getApplicationContext(), text, duration)
+			// showing the server response in an alert dialog
+			showAlert(result);
+
+			super.onPostExecute(result);
+		}
+
+	}
+	/**
+	 * Method to show alert dialog
+	 * */
+	private void showAlert(String message) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(message).setTitle("Response from Servers")
+				.setCancelable(false)
+				.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						// do nothing
+					}
+				});
+		AlertDialog alert = builder.create();
+		alert.show();
 	}
 
 	@Override
